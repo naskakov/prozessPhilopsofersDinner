219895f4bb5027dbea5dffda952137f3f0ecb51b
naskakov <naska.kov05@gmail.com>
2018-12-14 19:46:16 +0300

    philopsohers

---
+++ process/Mutex/Mutex/Mutex.cpp
@@ -0,0 +1,99 @@
+﻿// Mutex.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include<windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+#include<time.h>
+#include <atlconv.h>
+#include <fstream>
+
+using namespace std;
+
+char timebuf[80];
+SYSTEMTIME st;
+
+#define N 2 
+
+struct Philosopher      
+{
+	int number;
+};
+
+
+HANDLE forkMutex[N];    
+HANDLE mainMutex;
+
+void addlogsph(const char* data, int i) {
+	WaitForSingleObject(mainMutex, INFINITE);
+	GetLocalTime(&st);
+	sprintf_s(timebuf, "%d.%d.%d %d:%d:%d:%d", st.wDay, st.wMonth, st.wYear, st.wHour, st.wMinute, st.wSecond, st.wMilliseconds);
+	ofstream logfile("logfile.txt", ofstream::app);
+	if (data == "Error") {
+		logfile << timebuf << " " << data << endl;
+	}
+	else { logfile << timebuf << " " << "Pholosopher " << i << " " << data << endl; }
+	logfile.close();
+	ReleaseMutex(mainMutex);
+}
+
+void think(int i)      
+{
+	string buf;
+	buf = "thinking";
+	addlogsph(buf.c_str(), i);
+}
+
+void take_forks(int i)  
+{
+	DWORD dw = WaitForSingleObject(forkMutex[0], 100);
+	string buf;
+	switch (dw) {
+	case WAIT_OBJECT_0: {
+		buf = "take left fork";
+		addlogsph(buf.c_str(), i);
+		buf = "wait right fork";
+		addlogsph(buf.c_str(), i);
+		DWORD dw1 = WaitForSingleObject(forkMutex[1], 100);
+		switch (dw1) {
+		case WAIT_OBJECT_0:
+			buf = "is eating";
+			addlogsph(buf.c_str(), i);
+			Sleep(1000);
+			ReleaseMutex(forkMutex[1]);
+			ReleaseMutex(forkMutex[0]);
+			break;
+		case WAIT_TIMEOUT:
+			ReleaseMutex(forkMutex[0]);
+			break;
+		}
+		break; }
+	case WAIT_TIMEOUT:
+		break;
+	case WAIT_FAILED:
+		addlogsph("Error", i);
+		break;
+	}
+}
+
+
+int main(int argc, char *argv[])
+{
+	USES_CONVERSION;
+	Philosopher phil;
+	phil.number = atoi(argv[1]);
+
+	forkMutex[0] = OpenMutex(MUTEX_ALL_ACCESS, FALSE, A2W(argv[2]));
+	forkMutex[1] = OpenMutex(MUTEX_ALL_ACCESS, FALSE, A2W(argv[3]));
+	mainMutex = OpenMutex(MUTEX_ALL_ACCESS, FALSE, A2W(argv[4]));
+
+
+		while (1)    
+		{  
+			think(phil.number); 
+			take_forks(phil.number);   
+		}
+		
+}
---
+++ process/process.cpp
@@ -0,0 +1,61 @@
+﻿// process.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <windows.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include <cstdlib>
+#include <atlconv.h>
+
+
+using namespace std;
+
+#define N 5 
+HANDLE hMutex[N];
+HANDLE mainMutex;
+
+int main()
+{
+	USES_CONVERSION;
+	hMutex[0] = CreateMutex(NULL, FALSE, A2W("Mutex0"));
+	hMutex[1] = CreateMutex(NULL, FALSE, A2W("Mutex1"));
+	hMutex[2] = CreateMutex(NULL, FALSE, A2W("Mutex2"));
+	hMutex[3] = CreateMutex(NULL, FALSE, A2W("Mutex3"));
+	hMutex[4] = CreateMutex(NULL, FALSE, A2W("Mutex4"));
+	mainMutex = CreateMutex(NULL, FALSE, A2W("mainMutex"));
+	char lpszCommandLine[70];
+	
+	STARTUPINFO si;
+	SECURITY_ATTRIBUTES saProcess;
+	PROCESS_INFORMATION pi[5];
+
+	saProcess.nLength = sizeof(saProcess);
+	saProcess.lpSecurityDescriptor = NULL;
+	saProcess.bInheritHandle = TRUE;
+
+	ZeroMemory(&si, sizeof(STARTUPINFO));
+	si.cb = sizeof(STARTUPINFO);
+	for (int i = 1; i < 5; i++) {   
+		sprintf_s(lpszCommandLine, "Mutex.exe %d Mutex%d Mutex%d mainMutex", i, i - 1, i);
+		if (!CreateProcess(NULL, A2W(lpszCommandLine), &saProcess, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi[i - 1])) {
+			printf("CreateProcess finished with error code %d\n", GetLastError());
+			return 0;
+		}
+	}
+		sprintf_s(lpszCommandLine, "Mutex.exe %d Mutex%d Mutex%d mainMutex", 5, 4, 0);  
+		if (!CreateProcess(NULL, A2W(lpszCommandLine), &saProcess, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi[4])) {
+			printf("CreateProcess finished with error code %d\n", GetLastError());
+			return 0;
+		}
+	
+		system("pause");
+	for (int i = 0; i < 5; i++) {
+		CloseHandle(pi[i].hThread);
+		CloseHandle(pi[i].hProcess);
+		CloseHandle(hMutex[i]);
+	}
+	CloseHandle(mainMutex);
+}
